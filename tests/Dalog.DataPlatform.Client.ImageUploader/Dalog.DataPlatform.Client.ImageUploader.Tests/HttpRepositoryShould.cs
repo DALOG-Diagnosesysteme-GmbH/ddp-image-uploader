224ed4b3565d4cec4e450ebcb99963a4fbe43a9b
Camilo Paez <camilo.paez@dalog.net>
2024-02-02 13:10:42 +0100

    Testing coverage improved to default, zip, gzip and busybee images upload.

--- tests/Dalog.DataPlatform.Client.ImageUploader/Dalog.DataPlatform.Client.ImageUploader.Tests/HttpRepositoryShould.cs
+++ tests/Dalog.DataPlatform.Client.ImageUploader/Dalog.DataPlatform.Client.ImageUploader.Tests/HttpRepositoryShould.cs
@@ -36,7 +36,7 @@ namespace Dalog.DataPlatform.Client.ImageUploader.Tests
 
         [Trait("Category", "Unit Tests")]
         [Fact]
-        public async Task TestConnection_ReturnSuccess()
+        public async Task TestConnection_StatusCode204()
         {
             var sut = this._host.Services.GetRequiredService<HttpRepository>();
             Assert.NotNull(sut);
@@ -52,20 +52,92 @@ namespace Dalog.DataPlatform.Client.ImageUploader.Tests
 
         [Trait("Category", "Unit Tests")]
         [Fact]
-        public async Task UploadImages_NotReturnBadRequest()
+        public async Task UploadBusyBeeImages_NoStatusCode400()
         {
             var sut = this._host.Services.GetRequiredService<HttpRepository>();
             Assert.NotNull(sut);
             var settings = new UploadSettings()
             {
                 ApiKey = ApiKey,
-                Folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data"),
+                Folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "BusyBee"),
                 ImageType = ImageType.BusyBee,
                 Timeout = 10
             };
 
             var dataFolder = new DirectoryInfo(settings.Folder);
             var files = dataFolder.EnumerateFiles("*.sav", SearchOption.TopDirectoryOnly);
+            Assert.NotEmpty(files);
+            foreach (var file in files)
+            {
+                var response = await sut.UploadImageAsync(settings, file);
+                Assert.NotEqual(HttpStatusCode.BadRequest, response.StatusCode);
+            }
+        }
+
+        [Trait("Category", "Unit Tests")]
+        [Fact]
+        public async Task UploadDefaultImages_NoStatusCode400()
+        {
+            var sut = this._host.Services.GetRequiredService<HttpRepository>();
+            Assert.NotNull(sut);
+            var settings = new UploadSettings()
+            {
+                ApiKey = ApiKey,
+                Folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "Default"),
+                ImageType = ImageType.Default,
+                Timeout = 10
+            };
+
+            var dataFolder = new DirectoryInfo(settings.Folder);
+            var files = dataFolder.EnumerateFiles("*", SearchOption.TopDirectoryOnly);
+            Assert.NotEmpty(files);
+            foreach (var file in files)
+            {
+                var response = await sut.UploadImageAsync(settings, file);
+                Assert.NotEqual(HttpStatusCode.BadRequest, response.StatusCode);
+            }
+        }
+
+        [Trait("Category", "Unit Tests")]
+        [Fact]
+        public async Task UploadGzipImages_NoStatusCode400()
+        {
+            var sut = this._host.Services.GetRequiredService<HttpRepository>();
+            Assert.NotNull(sut);
+            var settings = new UploadSettings()
+            {
+                ApiKey = ApiKey,
+                Folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "GZip"),
+                ImageType = ImageType.GZip,
+                Timeout = 10
+            };
+
+            var dataFolder = new DirectoryInfo(settings.Folder);
+            var files = dataFolder.EnumerateFiles("*.gz", SearchOption.TopDirectoryOnly);
+            Assert.NotEmpty(files);
+            foreach (var file in files)
+            {
+                var response = await sut.UploadImageAsync(settings, file);
+                Assert.NotEqual(HttpStatusCode.BadRequest, response.StatusCode);
+            }
+        }
+
+        [Trait("Category", "Unit Tests")]
+        [Fact]
+        public async Task UploadZipImages_NoStatusCode400()
+        {
+            var sut = this._host.Services.GetRequiredService<HttpRepository>();
+            Assert.NotNull(sut);
+            var settings = new UploadSettings()
+            {
+                ApiKey = ApiKey,
+                Folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "Zip"),
+                ImageType = ImageType.Zip,
+                Timeout = 10
+            };
+
+            var dataFolder = new DirectoryInfo(settings.Folder);
+            var files = dataFolder.EnumerateFiles("*.zip", SearchOption.TopDirectoryOnly);
             Assert.NotEmpty(files);
             foreach (var file in files)
             {
